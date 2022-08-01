49de3f945379d5557dfb89476ea5210af77f0ca0
robrodric <roberto2.nechio@gmail.com>
2022-08-01 00:34:32 -0300

    adicionado perfil.js

---
+++ commands/perfil.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Canvas = require('canvas')
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('perfil')
+		.setDescription('Mostre seu perfil para todo mundo ver!'),
+	async execute(interaction) {
+        const canvas = Canvas.createCanvas(160, 300);
+		const context = canvas.getContext('2d');
+
+        const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+		await interaction.reply({ files: [attachment]});
+	},
+};
