91182ed2813b4bda12c5d5309c80ad8a1c666e53
robrodric <roberto2.nechio@gmail.com>
2022-08-01 01:08:02 -0300

    png

--- commands/perfil.js
+++ commands/perfil.js
@@ -8,7 +8,7 @@ module.exports = {
         const canvas = Canvas.createCanvas(160, 300);
 		const context = canvas.getContext('2d');
 
-        const background = await Canvas.loadImage('../imagens/wallpaper.jpg');
+        const background = await Canvas.loadImage('../imagens/background.png');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
