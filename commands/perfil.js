0e21594ace48b493ab2921332138887ad5aaafc5
robrodric <roberto2.nechio@gmail.com>
2022-08-01 00:47:46 -0300

    perfil.js

--- commands/perfil.js
+++ commands/perfil.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const Canvas = require('canvas')
 module.exports = {
 	data: new SlashCommandBuilder()
