5becf26ac5bf172b7546b2f29209c5430e031c81
robrodric <roberto2.nechio@gmail.com>
2022-08-01 01:19:40 -0300

    imagem

--- commands/perfil.js
+++ commands/perfil.js
@@ -3,12 +3,14 @@ const Canvas = require('canvas')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('perfil')
-		.setDescription('Mostre seu perfil para todo mundo ver!'),
+		.setDescription('Mostre seu perfil para todo mundo ver!')
+        .addUserOption(option => option.setName('usuário').setDescription('Mencione o usuário.').setRequired(false)),
 	async execute(interaction) {
+        const user = interaction.options.getUser('usuário') || interaction.user
         const canvas = Canvas.createCanvas(160, 300);
 		const context = canvas.getContext('2d');
 
-        const background = await Canvas.loadImage('../imagens/background.png');
+        const background = await Canvas.loadImage('/imagens/backgroung.png');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
